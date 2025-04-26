9b87d6f47adf6ad56a71da8365aaadce98e7974b
QiQPlayer <bronia_kov@mail.ru>
2025-04-26 19:53:13 +0300

    Add files via upload

--- My_ Game/Player.py
+++ My_ Game/Player.py
@@ -1,22 +1,28 @@
 import pygame as pg
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, hp):
         super().__init__()
         self.image = pg.image.load('texture/Player.png')
         self.rect = self.image.get_rect(center=(x, y))
-
-    def w(self):
-        self.rect.y -= 10
-
-    def a(self):
-        self.rect.x -= 10
-
-    def s(self):
-        self.rect.y += 10
-
-    def d(self):
-        self.rect.x += 10
+        self.HP = hp
 
     def update(self):
-        self.rect.y -= 5
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > 950:
+            self.rect.x = 950
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y > 750:
+            self.rect.y = 750
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            self.rect.y -= 3
+        if keys[pg.K_s]:
+            self.rect.y += 3
+        if keys[pg.K_d]:
+            self.rect.x += 3
+        if keys[pg.K_a]:
+            self.rect.x -= 3
