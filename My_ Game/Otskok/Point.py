57156efa8bf8347ad7c921366e3b972315d7a000
QiQPlayer <bronia_kov@mail.ru>
2025-05-17 19:53:33 +0300

    Delete My_ Game directory

--- My_ Game/Otskok/Point.py
+++
@@ -1,7 +0,0 @@
-import pygame as pg
-
-class Point(pg.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pg.image.load('texture/Point.png')
-        self.rect = self.image.get_rect(center=(x, y))
